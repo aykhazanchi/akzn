f4b7e047f6e4395c2fd1479e19e045c03392c7e8
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2022-08-21 00:53:16 +0200

    netlify integration

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -2,5 +2,6 @@ rm -rf ./public/akzn.me/*
 mkdir -p ./public/akzn.me
 hugo -D
 mv ./public/* ./public/akzn.me/
-rsync -av ./public/akzn.me akzn:~/
+netlify deploy
+#rsync -av ./public/akzn.me akzn:~/
 #scp -r ./public/akzn.me akzn.me:~/
