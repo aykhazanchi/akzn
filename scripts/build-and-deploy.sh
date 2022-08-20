5ff8bb7277865f8e8e3e5d09d0dc13e56ef8d524
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2022-08-21 00:57:45 +0200

    netlify prod deploy

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -3,6 +3,6 @@ mkdir -p ./public/akzn.me
 hugo -D
 mv ./public/* ./public/akzn.me/
 netlify deploy
-netlify deploy --prod
+#netlify deploy --prod
 #rsync -av ./public/akzn.me akzn:~/
 #scp -r ./public/akzn.me akzn.me:~/
